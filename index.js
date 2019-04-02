b8bd18a85663f42e0cb27dc61fe23e4cd72b6610
Kate Kim <katekim5809@gmail.com>
2019-04-01 22:05:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function logDriverNames(arr){
+  arr.forEach(el => console.log(el.name));
+};
+
+
+function logDriversByHometown(drivers, hometown){
+  logDriverNames(drivers.filter(driver => driver.hometown === hometown))
+};
+
+
+function driversByRevenue(drivers){
+  let arr = [...drivers]
+  return arr.sort((a,b) => a.revenue - b.revenue);
+}
+
+
+function driversByName(drivers){
+  let arr = drivers.slice()
+  return arr.sort((a,b) => a.name.localeCompare(b.name));
+};
+
+function totalRevenue(drivers){
+  const reducer = ((accumulator, driver) => accumulator + driver.revenue);
+  return drivers.reduce(reducer, 0);
+}
+
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers)/drivers.length
+};
